b8a5db931e664fd7935de69291d3e251033850c6
Jhony Freitas <jhony00._@hotmail.com>
2024-07-24 20:49:16 -0300

    feat: Criando do routes de products

--- src/routes/products.router.ts
+++ src/routes/products.router.ts
@@ -1,8 +1,11 @@
 import { Router } from "express"
+import { ProductsController } from '../controllers/products.controller'
 
 const productsRouter = Router();
+const productsController = new ProductsController();
 
 productsRouter.get('/', (req, res) => {
+    productsController.getProducts(req, res);
 })
 
 export { productsRouter }
